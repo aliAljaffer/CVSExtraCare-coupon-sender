1853bc9fc982076da2123abc1ad2ee9cd720016f
Ali Aljaffer <89468030+aliAljaffer@users.noreply.github.com>
2022-09-24 14:42:01 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+let sendToCardButtons = document.querySelectorAll("send-to-card-action > button");
+for(let i = 0; i<sendToCardButtons.length;i++){ 
+  sendToCardButtons[i].click();
+}
+console.log("Success");
